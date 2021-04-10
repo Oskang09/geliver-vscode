8c32f130c6d056dabebddc879fb2b42d43ed3f1e
Oskang09 <developer.oskang@gmail.com>
2021-04-11 03:34:44 +0800

    feat: worked with application

---
+++ README.md
@@ -0,0 +1,33 @@
+# geliver 
+
+Webview version of Geliver UI for support internally in vscode editor.
+
+## Features
+
+* Request, Response History
+* Collection and Preset 
+* App and Editor Theme
+* Import & Export Data ( currently not available in vscode webview )
+* Auto import endpoints from servers
+
+## Requirements
+
+* Only work with oscrud's applicaion
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `geliver.servers`: default servers setting will inject automatically
+* `geliver.app_theme`: default app theme 
+* `geliver.editor_theme`: default editor theme
+
+## Known Issues
+
+Currently webview is not able to import & export data, even tried local path believe it's some library not supported. 
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of geliver.
---
+++ extension.js
@@ -0,0 +1,63 @@
+const vscode = require('vscode');
+const path = require('path');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposable = vscode.commands.registerCommand('geliver.open_ui', async function () {
+		const config = vscode.workspace.getConfiguration('geliver');
+		const appTheme = config.get('app_theme', 'dark');
+		const editorTheme = config.get('editor_theme', 'dracula');
+		const servers = config.get('servers', []);
+		const distFolder = vscode.Uri.file(path.join(context.extensionPath, 'dist'));
+		const assetsFolder = vscode.Uri.file(path.join(context.extensionPath, 'assets'))
+		const panel = vscode.window.createWebviewPanel(
+			'geliver',
+			'Geliver',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				localResourceRoots: [distFolder, assetsFolder]
+			}
+		);
+
+		const distBase = panel.webview.asWebviewUri(distFolder).toString();
+		panel.webview.html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		
+		<head>
+		  <meta charset="UTF-8" />
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		  <title>Geliver</title>
+		
+		  <script>
+			window.mode = "vscode-webview";
+			window.base = "${distBase}";
+			window.appTheme = "${appTheme}";
+			window.editorTheme = "${editorTheme}";
+			window.servers = '${JSON.stringify(servers)}';
+		  </script>
+		  <script type="module" crossorigin src="${distBase}/assets/index.3577ab96.js"></script>
+			<link rel="modulepreload" href="${distBase}/assets/vendor.fa37ab51.js">
+			<link rel="stylesheet" href="${distBase}/assets/vendor.8c4ccc8e.css">
+			<link rel="stylesheet" href="${distBase}/assets/index.6a640632.css">
+		  <link rel="manifest" href="${distBase}/manifest.webmanifest"></head>
+		  <link href="https://cdn.jsdelivr.net/npm/jsoneditor/dist/jsoneditor.min.css" rel="stylesheet" type="text/css">
+		</head>
+		
+		<body>
+		  <div id="root"></div>
+		</body>
+		
+		</html>
+		`;
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() { }
+
+module.exports = { activate, deactivate }
